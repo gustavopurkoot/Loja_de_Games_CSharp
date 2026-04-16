54ebe28b6e0c376999e19d042520f2449e5952aa
gustavopurkoot <gustavopurkoot04053357@gmail.com>
2026-04-16 14:29:28 -0300

    Adiciona classe ConsoleGame

--- loja_de_games/Program.cs
+++ loja_de_games/Program.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 
 class Produto
 {
-    public String nome;
+    public string nome;
     public double preco;
-    public String Marca;
+    public ConsoleGame console;
     public int quantidade;
 
-    public Produto(String nome, double preco, String marca, int quantidade)
+    public Produto(string nome, double preco, ConsoleGame console, int quantidade)
     {
         this.nome = nome;
         this.preco = preco;
-        this.Marca = marca;
+        this.console = console;
         this.quantidade = quantidade;
     }
 
@@ -25,24 +25,33 @@ class Produto
     {
         Console.WriteLine("Nome: " + nome);
         Console.WriteLine("Preço: " + preco);
-        Console.WriteLine("Marca: " + Marca);
+        Console.WriteLine("Console: " + console.Nome);
         Console.WriteLine("Quantidade: " + quantidade);
         Console.WriteLine("Valor total do estoque: " + valorTotalEstoque());
     }
-
 }
+
+class ConsoleGame
+{
+    public string Nome;
+
+    public ConsoleGame(string nome)
+    {
+        Nome = nome;
+    }
+}
+
 class Program
 {
-
     static void Main()
     {
         List<Produto> produtos = new List<Produto>();
         int opcao = 0;
-        String nomeBuscar;
+        string nomeBuscar;
 
         do
         {
-            Console.WriteLine("1 - Cadastro de Produto");
+            Console.WriteLine("\n1 - Cadastro de Produto");
             Console.WriteLine("2 - Listar Produtos");
             Console.WriteLine("3 - Buscar Produto");
             Console.WriteLine("4 - Excluir Produto");
@@ -50,22 +59,23 @@ class Program
             Console.WriteLine("6 - Mostrar valor total geral do estoque");
             Console.WriteLine("7 - Encerrar");
 
-            Console.WriteLine("Digite a opção: ");
+            Console.Write("Digite a opção: ");
             opcao = int.Parse(Console.ReadLine());
 
             switch (opcao)
             {
                 case 1:
-                    Console.WriteLine("Digite o nome do produto:");
-                    String nome = Console.ReadLine()
-                    ;
-                    Console.WriteLine("Digite o preço do produto:");
+                    Console.Write("Digite o nome do produto: ");
+                    string nome = Console.ReadLine();
+
+                    Console.Write("Digite o preço do produto: ");
                     double preco = double.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Digite a marca do produto:");
-                    String marca = Console.ReadLine();
+                    Console.Write("Digite o console (PS5, Xbox, etc): ");
+                    string nomeConsole = Console.ReadLine();
+                    ConsoleGame console = new ConsoleGame(nomeConsole);
 
-                    Console.WriteLine("Digite a quantidade do produto:");
+                    Console.Write("Digite a quantidade do produto: ");
                     int quantidade = int.Parse(Console.ReadLine());
 
                     if (preco <= 0)
@@ -75,15 +85,16 @@ class Program
                     }
                     if (quantidade < 0)
                     {
-                        Console.WriteLine("A quantia do produto não pode ser menor que 0!");
+                        Console.WriteLine("Quantidade inválida!");
                         break;
                     }
 
-                    produtos.Add(new Produto(nome, preco, marca, quantidade));
-
+                    produtos.Add(new Produto(nome, preco, console, quantidade));
+                    Console.WriteLine("Produto cadastrado com sucesso!");
                     break;
+
                 case 2:
-                    Console.WriteLine("Digite 1 para listar todos os produtos ou 2 para listar por marca:");
+                    Console.WriteLine("Digite 1 para listar todos os produtos ou 2 para listar por console:");
                     int opcaoListar = int.Parse(Console.ReadLine());
 
                     if (opcaoListar == 1)
@@ -96,14 +107,14 @@ class Program
                     }
                     else if (opcaoListar == 2)
                     {
-                        Console.WriteLine("Digite a marca para filtrar:");
-                        string marcaBusca = Console.ReadLine().ToLower();
+                        Console.Write("Digite o console para filtrar: ");
+                        string consoleBusca = Console.ReadLine().ToLower();
 
                         bool encontrou = false;
 
                         foreach (Produto produto in produtos)
                         {
-                            if (produto.Marca.ToLower() == marcaBusca)
+                            if (produto.console.Nome.ToLower() == consoleBusca)
                             {
                                 Console.WriteLine("-------------");
                                 produto.Exibir();
@@ -113,7 +124,7 @@ class Program
 
                         if (!encontrou)
                         {
-                            Console.WriteLine("Nenhum produto encontrado com essa marca.");
+                            Console.WriteLine("Nenhum produto encontrado para esse console.");
                         }
                     }
                     else
@@ -121,31 +132,32 @@ class Program
                         Console.WriteLine("Opção inválida!");
                     }
                     break;
+
                 case 3:
-                    Console.WriteLine("Digite o nome do produto para buscar:");
+                    Console.Write("Digite o nome do produto para buscar: ");
                     nomeBuscar = Console.ReadLine();
+
                     bool encontrado = false;
+
                     foreach (Produto produto in produtos)
                     {
                         if (produto.nome.ToLower() == nomeBuscar.ToLower())
                         {
                             Console.WriteLine("Produto encontrado:");
                             produto.Exibir();
-
                             encontrado = true;
                             break;
                         }
-
                     }
+
                     if (!encontrado)
                     {
                         Console.WriteLine("Produto não encontrado.");
-                        break;
                     }
-
                     break;
+
                 case 4:
-                    Console.WriteLine("Digite o nome do produto: ");
+                    Console.Write("Digite o nome do produto para remover: ");
                     nomeBuscar = Console.ReadLine();
 
                     Produto remover = null;
@@ -168,10 +180,10 @@ class Program
                     {
                         Console.WriteLine("Produto não encontrado.");
                     }
-
                     break;
+
                 case 5:
-                    Console.WriteLine("Digite o nome do produto que deseja atualizar: ");
+                    Console.Write("Digite o nome do produto que deseja atualizar: ");
                     nomeBuscar = Console.ReadLine();
 
                     bool atualizado = false;
@@ -182,10 +194,10 @@ class Program
                         {
                             Console.WriteLine("Produto encontrado! Digite os novos dados:");
 
-                            Console.WriteLine("Novo nome:");
+                            Console.Write("Novo nome: ");
                             produto.nome = Console.ReadLine();
 
-                            Console.WriteLine("Novo preço:");
+                            Console.Write("Novo preço: ");
                             double novoPreco = double.Parse(Console.ReadLine());
                             if (novoPreco <= 0)
                             {
@@ -194,10 +206,11 @@ class Program
                             }
                             produto.preco = novoPreco;
 
-                            Console.WriteLine("Nova marca:");
-                            produto.Marca = Console.ReadLine();
+                            Console.Write("Novo console: ");
+                            string novoConsole = Console.ReadLine();
+                            produto.console = new ConsoleGame(novoConsole);
 
-                            Console.WriteLine("Nova quantidade:");
+                            Console.Write("Nova quantidade: ");
                             produto.quantidade = int.Parse(Console.ReadLine());
 
                             Console.WriteLine("Produto atualizado com sucesso!");
@@ -213,24 +226,23 @@ class Program
                     break;
 
                 case 6:
-                double totalGeral = 0;
+                    double totalGeral = 0;
 
-                foreach(Produto produto in produtos)
+                    foreach (Produto produto in produtos)
                     {
-                        totalGeral+= produto.valorTotalEstoque();
+                        totalGeral += produto.valorTotalEstoque();
                     }
+
                     Console.WriteLine("Valor total geral do estoque: " + totalGeral);
                     break;
 
                 default:
-
-                    Console.WriteLine("Opção Inválida!");
+                    Console.WriteLine("Opção inválida!");
                     break;
-
             }
 
-
         } while (opcao != 7);
-    }
 
+        Console.WriteLine("Encerrando...");
+    }
 }
